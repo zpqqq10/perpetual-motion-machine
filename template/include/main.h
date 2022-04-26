a09d76ab461edae911c58e0f106f165f0c9c9f70
N7Utb <765228482@qq.com>
2022-04-25 22:22:38 -0700

    add debug function

---
+++ template/include/main.h
@@ -0,0 +1,37 @@
+#ifndef __E9PATCH_H
+#define __E9PATCH_H
+
+#include <cstdarg>
+#include <cassert>
+#include <cstdio>
+
+#define NO_RETURN               __attribute__((__noreturn__))
+#define NO_INLINE               __attribute__((__noinline__))
+
+static inline void logImpl(char c)
+{
+    putchar(c);
+}
+static inline void logImpl(const char *s)
+{
+    fputs(s, stdout);
+}
+
+#define debug(msg, ...)                                                 \
+    do {                                                                \
+        if (__builtin_expect(option_debug, false))                      \
+            debugImpl((msg), ##__VA_ARGS__);                            \
+    } while (false)
+#define log(color, msg, ...)                                            \
+    do {                                                                \
+        logImpl(msg, ##__VA_ARGS__);                                    \
+    } while (false)
+
+
+extern bool option_debug;
+
+extern void warning(const char *msg, ...);
+extern void NO_RETURN error(const char *msg, ...);
+extern void debugImpl(const char *msg, ...);
+
+#endif
--- template/src/main.cpp
+++ template/src/main.cpp
@@ -2,11 +2,129 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <cstdarg>
+#include <getopt.h>
+
+#include <main.h>
+
 using namespace std;
+
+/*
+ * global options
+ */
+bool option_debug = false;
+
+/*
+ * extern function declaration
+ */
 extern int yyparse();
 
-int main()
+/*
+ * function declaration
+ */
+static void usage(FILE *stream, const char *progname);
+void warning(const char *msg, ...);
+void NO_RETURN error(const char *msg, ...);
+void debugImpl(const char *msg, ...);
+
+/*
+ * type declaration
+ */
+enum Option
 {
+	OPTION_DEBUG,
+	OPTION_HELP
+};
+
+
+int main(int argc, char** argv)
+{
+	const int req_arg = required_argument, /*opt_arg = optional_argument,*/
+			no_arg  = no_argument;
+	static const struct option long_options[] =
+    {
+        {"debug",         no_arg,  nullptr, OPTION_DEBUG},
+		{"help",          no_arg,  nullptr, OPTION_HELP},
+        {nullptr,         no_arg,  nullptr, 0}
+    }; 
+	while(true)
+	{
+		int idx;
+		int opt = getopt_long_only(argc, argv, "h", long_options, &idx);
+		if (opt < 0)
+			break;
+		switch(opt)
+		{
+			case OPTION_DEBUG:
+				option_debug = true;
+				break;
+			case OPTION_HELP:
+			case 'h':
+				usage(stdout, argv[0]);
+				return 0;
+		}
+	}
+	if(option_debug)
+		debug("test");
 	yyparse();
 	return 0;
+}
+
+static void usage(FILE *stream, const char *progname)
+{
+	fprintf(stream, "usage: %s [OPTIONS] \n\n"
+		"OPTIONS:\n"
+		"\n"
+		"\t--debug[=false]\n"
+		"\t\tEnable [disable] debug log messages.\n"
+		"\t\tDefault: false (disabled)\n"
+		"\n"
+		"\t--help, -h\n"
+        "\t\tPrint this help message.\n"
+        "\n",
+		progname);
+}
+
+/*  
+ * Report an error and exit.
+ */
+void NO_RETURN error(const char *msg, ...)
+{
+    fprintf(stderr, "%serror%s: ", "\33[31m", "\33[0m");
+
+    va_list ap;
+    va_start(ap, msg);
+    vfprintf(stderr, msg, ap);
+    va_end(ap);
+    
+    putc('\n', stderr);
+
+    _Exit(EXIT_FAILURE);
+}
+
+/*
+ * Print a warning message.
+ */
+void warning(const char *msg, ...)
+{
+    fprintf(stderr, "%swarning%s: ", "\33[33m",  "\33[0m");
+
+    va_list ap;
+    va_start(ap, msg);
+    vfprintf(stderr, msg, ap);
+    va_end(ap);
+    
+    putc('\n', stderr);
+}
+
+void debugImpl(const char *msg, ...)
+{
+    fprintf(stderr, "%sdebug%s: ","\33[35m", "\33[0m");
+
+    va_list ap;
+    va_start(ap, msg);
+    vfprintf(stderr, msg, ap);
+    va_end(ap);
+
+    putc('\n', stderr);
 }
